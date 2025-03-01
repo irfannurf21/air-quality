bf84950c64093bc28003a24d1f7ef6e2299ba6a7
Irfan Nur <irfan.nurfahrudin@gmail.com>
2025-03-01 15:30:21 +0700

    Update dashboard.py
    update dashboard karena perubahan notebook dan dataframe

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,13 +8,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(BASE_DIR, "main_data.csv")
 
-# Load dataset dengan pengecekan file
 try:
     df = pd.read_csv(data_path)
 except Exception as e:
     st.error(f"Error loading data: {e}")
     st.stop()
-# Pastikan format datetime benar
+
 df["date"] = pd.to_datetime(df[["year", "month", "day"]])
 
 # Fungsi menentukan musim
@@ -30,10 +29,14 @@ def get_season(month):
 
 df["season"] = df["month"].apply(get_season)
 
-
+# Definisi kategori kualitas udara berdasarkan PM2.5
+bins = [0, 50, 100, 150, 200, 300, float('inf')]
+labels = ["Baik", "Sedang", "Tidak Sehat (Sensitif)", "Tidak Sehat", "Sangat Tidak Sehat", "Berbahaya"]
+df["PM2.5_Category"] = pd.cut(df["PM2.5"], bins=bins, labels=labels, include_lowest=True)
 
 # Streamlit UI: Pilih lokasi
-selected_location = st.selectbox("Pilih Lokasi:", ["All"] + list(df["location"].unique()))
+df_locations = ["All"] + list(df["location"].unique())
+selected_location = st.selectbox("Pilih Lokasi:", df_locations)
 
 # Filter dataset berdasarkan lokasi
 if selected_location != "All":
@@ -46,16 +49,23 @@ seasonal_pm25 = filtered_df.groupby("season")["PM2.5"].mean().reset_index()
 
 # Plot rata-rata PM2.5 per musim
 st.subheader(f"Rata-rata PM2.5 per Musim ({selected_location})")
-
 fig, ax = plt.subplots(figsize=(8, 5))
-sns.barplot(x="season", y="PM2.5", data=seasonal_pm25, ax=ax)
+sns.barplot(x="season", y="PM2.5", data=seasonal_pm25, ax=ax, palette="coolwarm")
 ax.set_xlabel("Musim")
 ax.set_ylabel("PM2.5")
 ax.set_title("Rata-rata PM2.5 per Musim")
 st.pyplot(fig)
 
+# Visualisasi distribusi kategori PM2.5
+st.subheader(f"Distribusi Kategori PM2.5 ({selected_location})")
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.countplot(y=filtered_df["PM2.5_Category"], order=labels, palette="coolwarm", ax=ax)
+ax.set_xlabel("Jumlah Data")
+ax.set_ylabel("Kategori PM2.5")
+st.pyplot(fig)
+
 # Heatmap Korelasi PM2.5 dengan Faktor Cuaca
-st.subheader("Korelasi PM2.5 dengan Faktor Cuaca ")
+st.subheader("Korelasi PM2.5 dengan Faktor Cuaca")
 weather_corr = filtered_df[["PM2.5", "TEMP", "PRES", "DEWP", "RAIN", "WSPM"]].corr()
 fig, ax = plt.subplots(figsize=(6, 4))
 sns.heatmap(weather_corr, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5, ax=ax)
